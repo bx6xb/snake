6e256efa45b8dae7c2dd1a62bf83a1a2a6d8c340
Yan Yu <bx6xbChosen1@gmail.com>
2023-12-12 22:52:17 +0600

    added current score and record

--- src/ts/Snake.ts
+++ src/ts/Snake.ts
@@ -17,7 +17,25 @@ class Snake {
   private intervalId: ReturnType<typeof setInterval> = setInterval(() => {},
   10000)
   private snakePosition: number[]
-  private foodPostion: number[]
+  private foodPosition: number[] = [0, 0]
+  private scoreElement: HTMLElement
+  private score: number = 0
+  private recordElement: HTMLElement
+  private record: number = +(localStorage.getItem("record") ?? 0)
+
+  private directionIndexes: Dict = {
+    w: -1,
+    a: -1,
+    s: 1,
+    d: 1,
+  }
+
+  private oppositeDirections: StringStringDict = {
+    w: "s",
+    s: "w",
+    a: "d",
+    d: "a",
+  }
 
   constructor() {
     this.fields = Array.from(document.querySelectorAll(".field")!)
@@ -25,6 +43,8 @@ class Snake {
     this.overlay = document.querySelector(".overlay")!
     this.restartBtn = document.querySelector("#restart-btn")!
     this.snakePosition = [0, 0]
+    this.scoreElement = document.querySelector(".current-score")!
+    this.recordElement = document.querySelector(".record")!
 
     for (let i = 0; i <= this.size; i++) {
       this.fields.unshift(this.fields.splice(i, this.size + 1))
@@ -41,34 +61,29 @@ class Snake {
     })
 
     this.start()
-    this.foodPostion = this.createFood()
+    this.createFood()
+    this.recordElement.innerHTML = this.record.toString()
   }
 
-  private directionIndexes: Dict = {
-    w: -1,
-    a: -1,
-    s: 1,
-    d: 1,
-  }
-
-  private oppositeDirections: StringStringDict = {
-    w: "s",
-    s: "w",
-    a: "d",
-    d: "a",
-  }
-
-  private createFood = (): number[] => {
-    const foodPosition: number[] = [
+  private createFood = (): void => {
+    this.fields[this.foodPosition[0]][this.foodPosition[1]].innerHTML = ""
+    this.foodPosition = [
       this.getRandomNumber(0, this.size),
       this.getRandomNumber(0, this.size),
     ]
-    this.fields[foodPosition[0]][foodPosition[1]].innerHTML = "1"
-    return foodPosition
+    this.fields[this.foodPosition[0]][this.foodPosition[1]].innerHTML = "1"
   }
 
   private changeDirection = (direction: string): void => {
     this.direction = direction
+  }
+
+  private checkRecord = (): void => {
+    if (this.score > this.record) {
+      this.record = this.score
+      this.recordElement.innerHTML = this.record.toString()
+      localStorage.setItem("record", this.record.toString())
+    }
   }
 
   private getRandomNumber = (min: number, max: number): number => {
@@ -93,10 +108,16 @@ class Snake {
     this.start()
   }
 
+  private increaseScore = (): void => {
+    this.score++
+    this.scoreElement.innerHTML = this.score.toString()
+  }
+
   private gameOver = (): void => {
     clearInterval(this.intervalId)
     this.overlay.classList.add("show")
     this.restartBtn.addEventListener("click", this.restart)
+    this.score = 0
   }
 
   private moveSnake = (direction: string): void => {
@@ -109,6 +130,7 @@ class Snake {
         ) {
           this.y += this.directionIndexes[direction]
         } else {
+          this.checkRecord()
           this.gameOver()
         }
       } else {
@@ -118,13 +140,15 @@ class Snake {
         ) {
           this.x += this.directionIndexes[direction]
         } else {
+          this.checkRecord()
           this.gameOver()
         }
       }
       this.fields[this.y][this.x].classList.add("selected")
 
-      if (this.foodPostion.join("") === [this.y, this.x].join("")) {
-        console.log("food")
+      if (this.foodPosition.join("") === [this.y, this.x].join("")) {
+        this.increaseScore()
+        this.createFood()
       }
     }
   }
