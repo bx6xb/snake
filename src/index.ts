80a0269d75d8485ac5c1ddc14f7bc8307f863f6e
Yan Yu <bx6xbChosen1@gmail.com>
2023-12-28 19:01:22 +0600

    deleted overlay, buttons and styles for it. added new Snake.ts script

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import "./sass/style.sass"
 import "./ts/Snake.ts"
 import "./fonts/Comic Sans MS.ttf"
-import "./img/settings.png"
--- src/ts/Snake.ts
+++ src/ts/Snake.ts
@@ -1,166 +1,209 @@
-interface Dict {
-  [value: string]: number
-}
-
-interface StringStringDict {
-  [value: string]: string
+// Type number-number object
+interface OppositeKeysType {
+  [key: number]: number
 }
 
 class Snake {
-  private y: number = 0
-  private x: number = 0
-  private direction: string = "s"
-  private fields: any[]
-  private size: number
-  private overlay: HTMLElement
-  private restartBtn: HTMLElement
-  private intervalId: ReturnType<typeof setInterval> = setInterval(() => {},
-  10000)
-  private snakePosition: number[] = [0, 0]
+  // Init class variables
+  private yPos: number = 2 // Current position of snake's 'head' on the y axis
+  private xPos: number = 0 // Current position of snake's 'head' on the x axis
+  private snakePosition: number[][] = [
+    [2, 0],
+    [1, 0],
+    [0, 0],
+  ] // Position of each parts of snake
+  private direction: number = 83 // Current direction of snake
+  private directionChanged: boolean = false // Tracking direction changing
+  private gameFieldSize: number // Width and height of the game field
+  private intervalId: number = 0 // Id of setInterval
   private foodPosition: number[] = [0, 0]
-  private scoreElement: HTMLElement
-  private score: number = 0
-  private recordElement: HTMLElement
-  private record: number = +(localStorage.getItem("record") ?? 0)
-  private directionIsChanged: boolean = false
+  private foodEaten: boolean = false
+  private currentScore: number = 0 // Current user score
+  private bestScore: number = +(localStorage.getItem("score") ?? 0)
 
-  private directionIndexes: Dict = {
-    w: -1,
-    a: -1,
-    s: 1,
-    d: 1,
+  // Key codes object
+  private oppositeKeys: OppositeKeysType = {
+    87: 83, // 83 - s (down direction)
+    83: 87, // 87 - w (up direction)
+    68: 65, // 65 - a (left direction)
+    65: 68, // 68 - d (rigth direction)
   }
 
-  private oppositeDirections: StringStringDict = {
-    w: "s",
-    s: "w",
-    a: "d",
-    d: "a",
-  }
+  // Page elements
+  private cells: Element[][] // Cells of the game field
+  private currentScoreElement: Element
+  private bestScoreElement: Element
 
   constructor() {
-    this.fields = Array.from(document.querySelectorAll(".field")!)
-    this.size = this.fields.length ** 0.5 - 1
-    this.overlay = document.querySelector(".overlay")!
-    this.restartBtn = document.querySelector("#restart-btn")!
-    this.snakePosition = [0, 0]
-    this.scoreElement = document.querySelector(".current-score")!
-    this.recordElement = document.querySelector(".record")!
-
-    for (let i = 0; i <= this.size; i++) {
-      this.fields.unshift(this.fields.splice(i, this.size + 1))
+    // Creating two-dimensional cells array
+    this.cells = []
+    const gameCells: Element[] = Array.from(document.querySelectorAll(".cell")!)
+    this.gameFieldSize = Math.sqrt(gameCells.length)
+    for (let i = 0; i < this.gameFieldSize; i++) {
+      this.cells.push(gameCells.splice(0, this.gameFieldSize))
     }
-    this.fields.reverse()
 
-    document.addEventListener("keypress", (e) => {
-      if (
-        e.key in this.oppositeDirections &&
-        this.direction !== this.oppositeDirections[e.key] &&
-        !this.directionIsChanged
-      ) {
-        this.changeDirection(e.key)
+    // Getting elements which shows user scores
+    this.currentScoreElement = document.querySelector(".current-score")!
+    this.bestScoreElement = document.querySelector(".record")!
+
+    // Setting best user score
+    this.bestScoreElement.innerHTML = this.bestScore.toString()
+
+    // Creating food and snake on the game field
+    this.createFood()
+    this.paintSnake()
+
+    // Creating interval
+    this.intervalId = window.setInterval(() => {
+      this.directionChanged = false
+      this.eraseSnake()
+      this.updatePosition()
+      this.paintSnake()
+    }, 150 /* snake speed */)
+
+    // Creating event listener for document
+    document.addEventListener("keydown", this.changeDirection)
+  }
+
+  private changeDirection = (e: KeyboardEvent): void => {
+    if (
+      [87, 83, 68, 65].includes(e.keyCode) &&
+      this.direction !== this.oppositeKeys[e.keyCode] &&
+      !this.directionChanged
+    ) {
+      this.direction = e.keyCode
+    }
+    this.directionChanged = true
+  }
+
+  // Snake logic functions
+  private eraseSnake = (): void => {
+    this.snakePosition.forEach((val) => {
+      const [y, x]: number[] = val
+      this.cells[y][x].classList.remove("snake")
+    })
+  }
+
+  private paintSnake = (): void => {
+    this.snakePosition.forEach((val) => {
+      const [y, x]: number[] = val
+      this.cells[y][x].classList.add("snake")
+    })
+  }
+
+  private updatePosition = (): void => {
+    if (this.direction === 83 && this.yPos + 1 < this.gameFieldSize) {
+      this.yPos += 1
+      if (!this.foodEaten) this.snakePosition.pop()
+      this.snakePosition.unshift([this.yPos, this.xPos])
+    } else if (this.direction === 87 && this.yPos - 1 >= 0) {
+      this.yPos -= 1
+      if (!this.foodEaten) this.snakePosition.pop()
+      this.snakePosition.unshift([this.yPos, this.xPos])
+    } else if (this.direction === 68 && this.xPos + 1 < this.gameFieldSize) {
+      this.xPos += 1
+      if (!this.foodEaten) this.snakePosition.pop()
+      this.snakePosition.unshift([this.yPos, this.xPos])
+    } else if (this.direction === 65 && this.xPos - 1 >= 0) {
+      this.xPos -= 1
+      if (!this.foodEaten) this.snakePosition.pop()
+      this.snakePosition.unshift([this.yPos, this.xPos])
+    } else {
+      this.gameOver()
+    }
+    this.checkCollision()
+    this.foodEaten = false
+    this.checkFood()
+  }
+
+  private checkCollision = (): void => {
+    this.snakePosition.forEach((val, i) => {
+      if (i !== 0) {
+        const [y, x]: number[] = val
+        if (x === this.xPos && y === this.yPos) {
+          this.gameOver()
+        }
       }
     })
-
-    this.start()
-    this.createFood()
-    this.recordElement.innerHTML = this.record.toString()
   }
 
-  private createFood = (): void => {
-    this.fields[this.foodPosition[0]][this.foodPosition[1]].innerHTML = ""
-    this.foodPosition = [
-      this.getRandomNumber(0, this.size),
-      this.getRandomNumber(0, this.size),
-    ]
-    this.fields[this.foodPosition[0]][
-      this.foodPosition[1]
-    ].innerHTML = `<div class="food"></div>`
-  }
-
-  private changeDirection = (direction: string): void => {
-    this.directionIsChanged = true
-    this.direction = direction
-  }
-
-  private checkRecord = (): void => {
-    if (this.score > this.record) {
-      this.record = this.score
-      this.recordElement.innerHTML = this.record.toString()
-      localStorage.setItem("record", this.record.toString())
+  private checkRecord = () => {
+    if (this.currentScore > this.bestScore) {
+      this.bestScoreElement.innerHTML = this.currentScore.toString()
+      localStorage.setItem("score", this.currentScore.toString())
     }
   }
 
+  // Game logic functions
   private getRandomNumber = (min: number, max: number): number => {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
-  private start = (): void => {
-    this.fields[this.y][this.x].classList.remove("snake")
-    this.y = 7
-    this.x = 2
-    this.snakePosition = [this.y, this.x]
-    this.direction = "d"
-    this.fields[this.y][this.x].classList.add("snake")
+  private createFood = (): void => {
+    let c = true
+    while (c) {
+      c = false
+      const yPosFood: number = this.getRandomNumber(0, this.gameFieldSize - 1)
+      const xPosFood: number = this.getRandomNumber(0, this.gameFieldSize - 1)
 
-    this.intervalId = setInterval(() => {
-      this.directionIsChanged = false
-      this.moveSnake(this.direction)
-    }, 200)
-  }
-
-  private restart = (): void => {
-    this.restartBtn.removeEventListener("click", this.restart)
-    this.overlay.classList.remove("show")
-    this.start()
-  }
-
-  private increaseScore = (): void => {
-    this.score++
-    this.scoreElement.innerHTML = this.score.toString()
-  }
-
-  private gameOver = (): void => {
-    clearInterval(this.intervalId)
-    this.overlay.classList.add("show")
-    this.restartBtn.addEventListener("click", this.restart)
-    this.score = 0
-    this.scoreElement.innerHTML = "0"
-  }
-
-  private moveSnake = (direction: string): void => {
-    if (["w", "a", "s", "d"].includes(direction)) {
-      this.fields[this.y][this.x].classList.remove("snake")
-      if (["w", "s"].includes(direction)) {
-        if (
-          this.y + this.directionIndexes[direction] >= 0 &&
-          this.y + this.directionIndexes[direction] <= this.size
-        ) {
-          this.y += this.directionIndexes[direction]
-        } else {
-          this.checkRecord()
-          this.gameOver()
+      this.snakePosition.forEach((val) => {
+        const [y, x] = val
+        if (y === yPosFood && x === xPosFood) {
+          c = true
         }
-      } else {
-        if (
-          this.x + this.directionIndexes[direction] >= 0 &&
-          this.x + this.directionIndexes[direction] <= this.size
-        ) {
-          this.x += this.directionIndexes[direction]
-        } else {
-          this.checkRecord()
-          this.gameOver()
-        }
-      }
-      this.snakePosition = [this.y, this.x]
-      this.fields[this.y][this.x].classList.add("snake")
+      })
 
-      if (this.foodPosition.join("") === [this.y, this.x].join("")) {
-        this.increaseScore()
-        this.createFood()
+      if (!c) {
+        this.foodPosition = [yPosFood, xPosFood]
+        this.cells[yPosFood][xPosFood].classList.add("food")
       }
     }
   }
+
+  private checkFood = (): void => {
+    if (this.snakePosition[0].join("") === this.foodPosition.join("")) {
+      this.foodEaten = true
+      this.clearFood()
+      this.createFood()
+
+      this.currentScore += 1
+      this.currentScoreElement.innerHTML = this.currentScore.toString()
+      this.checkRecord()
+    }
+  }
+
+  private clearFood = (): void => {
+    const [yPosFood, xPosFood] = this.foodPosition
+    this.cells[yPosFood][xPosFood].classList.remove("food")
+  }
+
+  private startGame = (): void => {
+    this.currentScore = 0
+    this.currentScoreElement.innerHTML = this.currentScore.toString()
+    this.yPos = 2
+    this.xPos = 0
+    this.snakePosition = [
+      [2, 0],
+      [1, 0],
+      [0, 0],
+    ]
+    this.direction = 83
+    this.createFood()
+  }
+
+  private gameOver = (): void => {
+    console.log("Game is over!")
+    this.clearFood()
+    this.startGame()
+  }
+
+  public deinit = (): void => {
+    this.eraseSnake()
+    document.removeEventListener("keydown", this.changeDirection)
+    clearInterval(this.intervalId)
+    this.clearFood()
+  }
 }
 
-const snake: Snake = new Snake()
+const snake = new Snake()
